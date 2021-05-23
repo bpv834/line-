7a7a0bff261e428c79c89f184d35258bd7549568
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-05-23 19:38:44 +0900

    초기 커밋.

---
+++ line 구조체의 정의 및 사용/line 구조체의 정의 및 사용.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <string>
+#include <math.h>
+
+struct point {
+	int x, y;
+};
+
+struct line {
+	struct point start, end;
+};
+
+void pr_p(const struct point* ptr) {
+	printf("(%d %d)", ptr->x, ptr->y);
+}
+double get_lenth(const struct line* ptr) {
+	long int dx = ptr->end.x - ptr->start.x;
+	long int dy = ptr->end.y - ptr->start.y;
+	return sqrt(dx * dx + dy * dy);
+}
+
+int main() {
+	struct line a {
+		{10, 20}, { 30,40 }
+	};
+	printf("직선 정보:");
+	pr_p(&a.start);
+	pr_p(&a.end);
+
+	printf("\n길이:%f\n", get_lenth(&a));
+
+	
+
+	
+}
+
